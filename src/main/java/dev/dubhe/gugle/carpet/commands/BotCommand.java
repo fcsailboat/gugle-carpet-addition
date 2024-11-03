492632f593a37d79f538eb92ddc5532cef8dc605
弭忧萌虾 <2211479860@qq.com>
2024-11-03 22:40:11 +0800

    删除无用import

--- src/main/java/dev/dubhe/gugle/carpet/commands/BotCommand.java
+++ src/main/java/dev/dubhe/gugle/carpet/commands/BotCommand.java
@@ -36,7 +36,6 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 public class BotCommand {
