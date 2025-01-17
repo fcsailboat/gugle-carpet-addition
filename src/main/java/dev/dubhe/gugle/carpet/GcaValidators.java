e249712030fe2a72c22a044f5b4233d0e69b5a40
fcsailboat <3286637847@qq.com>
2025-01-17 17:24:25 +0800

    保持/here与Carpet AMS Addition兼容

--- src/main/java/dev/dubhe/gugle/carpet/GcaValidators.java
+++ src/main/java/dev/dubhe/gugle/carpet/GcaValidators.java
@@ -1,13 +1,17 @@
 package dev.dubhe.gugle.carpet;
 
 import carpet.api.settings.CarpetRule;
+import carpet.api.settings.Rule;
 import carpet.api.settings.Validator;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.commands.CommandSourceStack;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
 public class GcaValidators {
+    public static final boolean CARPET_AMS_ADDITION = FabricLoader.getInstance().isModLoaded("carpet-ams-addition");
+
     public static class EnderChest extends Validator<String> {
         public static final List<String> OPTIONS = List.of("true", "false", "ender_chest");
 
@@ -18,6 +22,13 @@ public class GcaValidators {
 
         public String description() {
             return "Can be limited to 'ender_chest' for use EnderChest open only, true/false for open directly/unable";
+        }
+    }
+
+    public static class CarpetAmsAdditionLoaded implements Rule.Condition {
+        @Override
+        public boolean shouldRegister() {
+            return !CARPET_AMS_ADDITION;
         }
     }
 }
